39552415f3e18fb9017e85f8d29c50571aabec6f
wischmcj <wischmcj@miamioh.edu>
2021-06-02 19:13:17 +0000

    Created Angular App Skeleton

---
+++ src/app/Specs/00_demo.spec.ts
@@ -0,0 +1,20 @@
+describe('calculator', () => {
+
+    describe('addition', () => {
+
+        it('can add two numbers', () => {
+            const a = 10;
+            const b = 20;
+            const answer = a + b;
+            expect(answer).toBe(30);
+        })
+
+        it('using let with type in type script', () => {
+            let x: Number;
+            x = 3.14;
+            x = 12;
+
+        })
+
+    })
+})
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,17 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'few100'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('few100');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('few100 app is running!');
-  });
+  /*
+    it(`should have as title 'few100'`, () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.title).toEqual('few100');
+    });
+  
+    it('should render title', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement;
+      expect(compiled.querySelector('.content span').textContent).toContain('few100 app is running!');
+    });*/
 });
